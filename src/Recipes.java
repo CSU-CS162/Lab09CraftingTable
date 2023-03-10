381692cb9b664fcbff387b256ce72cadddb5e616
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-03-10 15:01:01 -0700

    Update Recipes.java

--- src/Recipes.java
+++ src/Recipes.java
@@ -9,12 +9,17 @@ public class Recipes {
      * Each individual recipe is stored as a recipe object, which is then stored in a list of recipes.
      * This uses a file to populate the recipe book.
      */
-    public Recipes(String filename) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File(filename));
-        while(scan.hasNextLine()) {
-            String name = scan.next();
-            String[] ingredients = scan.next().split(",");
-            recipes.add(new Recipe(name, ingredients));
+    public Recipes(String filename) {
+        try {
+            Scanner scan = new Scanner(new File(filename));
+            while (scan.hasNextLine()) {
+                String name = scan.next();
+                String[] ingredients = scan.next().split(",");
+                recipes.add(new Recipe(name, ingredients));
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println(Arrays.toString(e.getStackTrace()));
         }
     }
 }
