6c0185fc79ebb4860c20b9194f86848b481a653a
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-03-20 12:14:58 -0600

    Update Recipe.java

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,12 +1,14 @@
 public class Recipe {
     String name;
-    String[][] recipe = new String[3][3];
+    String[][] recipe;
 
     /**
      * Creates a 2D array with the ingredients needed to craft the object and saves the name of the object
      */
     public Recipe(String name, String[] ingredients){
         this.name = name;
+        this.recipe = new String[3][3];
+        
         int index = 0;
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
