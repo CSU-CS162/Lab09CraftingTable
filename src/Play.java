ff707488cdad6f0f42e486ed6dc4e062394d05c6
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-03-20 12:19:29 -0600

    Update Play.java

--- src/Play.java
+++ src/Play.java
@@ -3,24 +3,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Play {
-    String[][] CraftingTable = new String[3][3];
-    Scanner input = new Scanner(System.in);
-    CraftingTable Crafting = new CraftingTable();
-    Recipes RecipeBook;
+    String[][] craftingTable;
+    Scanner input;
+    CraftingTable crafting;
+    Recipes recipeBook;
 
-    public Play(String filename) throws FileNotFoundException {
+    public Play(String filename) {
+        craftingTable = new String[3][3];
+        input = new Scanner(System.in);
+        crafting = new CraftingTable();
+
         for(int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
-                CraftingTable[i][j] = "";
+                craftingTable[i][j] = "";
             }
         }
-        RecipeBook = new Recipes(filename);
+        recipeBook = new Recipes(filename);
     }
 
     /**
      * returns a string containing the options
      */
-    public static String Options(){
+    public static String options(){
         return  "A: Add Item\n" +
                 "R: Remove Item\n" +
                 "V: View Crafting Table\n" +
@@ -33,29 +37,29 @@ public class Play {
     /**
      * gets the item, row, and column, then calls the AddItem function
      */
-    public void Add(){
+    public void add(){
         System.out.println("What would you like to add?");
         String item = input.nextLine();
         System.out.println("Where would you like to add it?\nRow: ");
         int row = Integer.parseInt(input.nextLine());
         System.out.println("Column: ");
         int column = Integer.parseInt(input.nextLine());
-        Crafting.AddItem(CraftingTable, item, row, column);
+        crafting.addItem(craftingTable, item, row, column);
     }
 
     /**
      * gets the item to be removed, then calls the Remove function
      */
-    public void Remove(){
+    public void remove(){
         System.out.println("What item would you like to remove?");
         String item = input.nextLine();
-        Crafting.RemoveItem(CraftingTable, item);
+        crafting.removeItem(craftingTable, item);
     }
 
     /**
      * prints the crafting table
      */
-    public void View(String[][] table){
+    public void view(String[][] table){
         System.out.println("_ _ _ _ _ _ _ _ _ _ _ _");
         for (String[] rows : table){
             System.out.print("| ");
@@ -70,25 +74,25 @@ public class Play {
     /**
      * prints information in different recipes
      */
-    public void RecipeList(){
+    public void recipeList(){
         System.out.println("The Recipes are as Follows: ");
         int counter = 1;
-        for (Recipe r : RecipeBook.recipes){
+        for (Recipe r : recipeBook.recipes){
             System.out.println(String.format("%d: %s", counter, r.name));
             counter++;
         }
         System.out.println("Please enter the number of the recipe you would like to see: ");
         int index = Integer.parseInt(input.nextLine()) - 1;
-        View(RecipeBook.recipes.get(index).recipe);
+        view(recipeBook.recipes.get(index).recipe);
 
     }
 
     /**
      * attempts to craft an item using the recipe book and the current crafting table
      */
-    public String Craft(){
-        for (Recipe r : RecipeBook.recipes){
-            if (Arrays.deepEquals(r.recipe, CraftingTable)){
+    public String craft(){
+        for (Recipe r : recipeBook.recipes){
+            if (Arrays.deepEquals(r.recipe, craftingTable)){
                 return "You Crafted: " + r.name;
             }
         }
@@ -98,22 +102,22 @@ public class Play {
     public void PlayGame(){
         System.out.println("Welcome to the Crafting Table Simulator!\nWhat would you like to do?");
         while (true){
-            System.out.println(Options());
+            System.out.println(options());
             String userInput = input.nextLine();
             if(userInput.equalsIgnoreCase("a")){ //add item
-                Add();
+                add();
             }
             else if(userInput.equalsIgnoreCase("r")){ //remove item
-                Remove();
+                remove();
             }
             else if (userInput.equalsIgnoreCase("v")){ //view table
-                    View(CraftingTable);
+                    view(craftingTable);
             }
             else if (userInput.equalsIgnoreCase("c")){ //craft
-                    System.out.println(Craft());
+                    System.out.println(craft());
             }
             else if (userInput.equalsIgnoreCase("p")){ //display recipes
-                RecipeList();
+                recipeList();
             }
             else if (userInput.equalsIgnoreCase("x")){ //exit
                 break;
@@ -127,7 +131,7 @@ public class Play {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        Play craft = new Play("recipes.txt"); //this loads up the recipes file for our crafting and starts the program
+        Play craft = new Play("C:\\Users\\inter\\Desktop\\CS College programming\\MinecraftLab\\src\\recipes.txt"); //this loads up the recipes file for our crafting and starts the program
         craft.PlayGame();
     }
 }
